1a75b95bd7be9ac08b3efd205f56bd274e662536
Addin Hadi Rizal <addinhadi10403@gmail.com>
2025-08-12 14:21:54 +0700

    Set-Up GIN

---
+++ Situs-Forum/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// router.Run() // listen and serve on 0.0.0.0:8080
+	router.Run(":9999") // listen and serve on 9999 port
+}
